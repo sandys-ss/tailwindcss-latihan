74cacc65ff130a724c7c6fc2cc556a6e59aec33d
sandys-ss <sandysyam@gmail.com>
2021-06-01 17:48:41 +0800

    optimal production

---
+++ 05-optimal-production/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './dist/index.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#12E7B4",
+          DEFAULT: "#45A4E9",
+          dark: "#E71212",
+        }
+      },
+      fontFamily: {
+        headline: "Tangerine",
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+      fontSize: ["hover"],
+    },
+  },
+  plugins: [],
+}
